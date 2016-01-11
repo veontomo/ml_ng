94a97950ba4a8977b0df5e4c4f5d856d5b454641
Mario Rossi <veontomo@gmail.com>
2016-01-11 21:58:39 +0100

    feat: implement the predict function

--- assignments/machine-learning-ex2/ex2/predict.m
+++ assignments/machine-learning-ex2/ex2/predict.m
@@ -14,11 +14,13 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
-
-
+ 
+ tmp = sigmoid(X * theta);
+ for i=1:m
+  if (tmp(i)>=0.5) 
+   p(i)=1;
+ endif
+ endfor
 
 
 % =========================================================================
