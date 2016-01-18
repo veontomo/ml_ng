f21fcdafc215bdaf140924a71b616e9077b0704c
Mario Rossi <veontomo@gmail.com>
2016-01-18 07:45:54 +0100

    playing eith color plots

--- assignments/machine-learning-ex3/ex3/colorPlot.m
+++ assignments/machine-learning-ex3/ex3/colorPlot.m
@@ -1,7 +1,8 @@
 %%% http://blogs.mathworks.com/pick/2008/08/15/colors-for-your-multi-line-plots/
 
-data = Theta1;
+data = prob;
 rows = size(data, 1);
+cols = size(data, 2);
 colorSet = varycolor(rows);
 set(gca, 'ColorOrder', colorSet);
-plot(1:rows, data)
+plot(1:cols, data(1:20, :), 'x')
