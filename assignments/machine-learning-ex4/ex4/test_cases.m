b62af4b0e6fdc22d9cf27ad07d1fae2806a3af0f
Mario Rossi <veontomo@gmail.com>
2016-01-19 15:04:42 +0100

    feat: implement the backpropagation algo

--- assignments/machine-learning-ex4/ex4/test_cases.m
+++ assignments/machine-learning-ex4/ex4/test_cases.m
@@ -3,7 +3,7 @@
 
 % all test cases: 
 % https://www.coursera.org/learn/machine-learning/discussions/0SxufTSrEeWPACIACw4G5w
-
+clc;
 il = 2;              % input layer
 hl = 2;              % hidden layer
 nl = 4;              % number of labels
@@ -14,7 +14,7 @@ lambda = 4;
 [J_actual grad_actual] = nnCostFunction(nn, il, hl, nl, X, y, lambda)
 
 J_expected = 19.474
-grad_expected = [0.76614; 0.97990; 0.37246;0.49749;0.64174;0.74614;0.88342;0.56876;0.58467;0.59814;1.92598;1.94462;1.98965;2.17855;2.47834;2.50225;2.52644;2.72233];
+grad_expected = [0.766138 0.979897 -0.027540 -0.035844 -0.024929 -0.053862 0.883417 0.568762 0.584668 0.598139 0.459314 0.344618 0.256313 0.311885 0.478337 0.368920 0.259771 0.322331]';
 
 error_J = abs((1 - J_actual/J_expected));
 error_grad = abs((1 - grad_actual ./ grad_expected));
