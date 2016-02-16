34359da392f3be264923799585be67c20918420c
Mario Rossi <veontomo@gmail.com>
2016-02-16 22:20:36 +0100

    feat: play with 3-d plot

--- assignments/evenodd2/cost.m
+++ assignments/evenodd2/cost.m
@@ -1,9 +1,9 @@
 function [J grad] = cost(X, Y, Theta)
-  fprintf("X = ");
-  fprintf("%u ", X(:, 2));
-  fprintf("\nTheta = ");
-  fprintf("%2.4f ", Theta);
-  fprintf("\n");
+%  fprintf("X = ");
+%  fprintf("%u ", X(:, 2));
+%  fprintf("\nTheta = ");
+%  fprintf("%2.4f ", Theta);
+%  fprintf("\n");
   h = sigmoid(X * Theta'); % column
   m = size(X, 1);
   J = (- Y' * log(h) - (1 - Y)' * log(1-h))/m;
--- assignments/evenodd2/script.m
+++ assignments/evenodd2/script.m
@@ -1,12 +1,17 @@
- X = [1 2; 1 4; 1 5; 1 1; 1 9];
- Y = [1 1 0 0 0]';
- theta_init = [1 1];
+X = [1 2; 1 4; 1 5; 1 1; 1 9];
+Y = [1 1 0 0 0]';
+theta_init = [0 0];
 
- 
 
 % Set Options
-options = optimset('GradObj', 'on', 'MaxIter', 400);
+options = optimset('GradObj', 'on', 'MaxIter', 1000);
 
 % Optimize
 [theta, J, exit_flag] = fminunc(@(Theta)(cost(X, Y, Theta)), theta_init, options);
 
+tx = linspace (-2, 2, 4)';
+ty = linspace (-1, 1, 3)';
+[xx, yy] = meshgrid (tx, ty);
+r = sqrt (xx .^ 2 + yy .^ 2) + eps;
+tz = sin (r) ./ r;
+mesh (tx, ty, tz);
