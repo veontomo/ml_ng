39224ad964e9c70a5b935c0d68eb5ec8a0bf6019
Mario Rossi <veontomo@gmail.com>
2016-02-16 17:00:10 +0100

    feat: change the start point in order to get rid of division-by-zero warning

--- assignments/evenodd2/script.m
+++ assignments/evenodd2/script.m
@@ -1,14 +1,8 @@
  X = [1 2; 1 4; 1 5; 1 1; 1 9];
- Y = [1 1 -1 -1 -1]';
- theta_init = [2 7.6];
+ Y = [1 1 0 0 0]';
+ theta_init = [1 1];
 
  
- 
- h = sigmoid(X * Theta') % column
-  m = size(X, 1)
-  J = (- Y' * log(h) - (1 - Y)' * log(1-h))/m
-  grad = (h - Y)' * X/m
-
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
