e6e9112abd906e88ee4b7065b4607ea425158b78
Mario Rossi <veontomo@gmail.com>
2016-02-17 22:27:26 +0100

    feat: use normalized features

--- assignments/evenodd2/script.m
+++ assignments/evenodd2/script.m
@@ -1,7 +1,7 @@
 %%% Generate training examples
 %%% The training set consists of pair-wise different integer numbers
 clc;clear;
-A = 1000; % the number of the training examples
+A = 4000; % the number of the training examples
 Data = zeros(A, 1);
 maxLoopIter = 3;  % the maximal number of iterations to pick up a random 
                   % integer number before widening the range
@@ -25,15 +25,15 @@ endfor;
 Y = mod(Data, 2);
 
 %%% single-parameter model
-
-X = [ones(size(Data, 1), 1), Data];
+[mu range DataNorm] = normalize(Data);
+X = [ones(size(Data, 1), 1), DataNorm];
 
 
 % Training the model
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 theta_init = unifrnd(0, 1, 1, size(X, 2));
 
-trainingSize = 200; % the number of examples to train on
+trainingSize = 2000; % the number of examples to train on
 Jpool = zeros(1, trainingSize);
 for i = 5:trainingSize
   [theta, J, exit_flag] = fminunc(@(Theta)(cost(X(1:i, :), Y(1:i, :), Theta)), theta_init, options);
@@ -56,14 +56,14 @@ Fscore = 2*Prec*Rec/(Prec + Rec)
 
 
 %%%%%%%%%%%%% another model
-X = [ones(size(Data, 1), 1), Data, mod(Data, 2)];
+X = [ones(size(Data, 1), 1), DataNorm, mod(Data, 2)];
 theta_init = unifrnd(-2, 2, 1, size(X, 2));
 
 
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 
-trainingSize = 100; % the number of examples to train on
+trainingSize = 500; % the number of examples to train on
 Jpool = zeros(1, trainingSize);
 for i = 1:trainingSize
   [theta, J, exit_flag] = fminunc(@(Theta)(cost(X(1:i, :), Y(1:i, :), Theta)), theta_init, options);
@@ -72,15 +72,7 @@ endfor
 
 plot(1:trainingSize, Jpool)
 
+
 yPredicted = X * theta' > 0;
 [tp tn fp fn] = classifyPredictions(Y, yPredicted)
 
-
-
-
-
-h = sigmoid(x * theta') % column
- m = size(x, 1)
- J = -sum(log(h(find(y==1)))) - sum(log(1-h(find(y==0))))
- J = J/m
- grad = (h - Y)' * X/m
