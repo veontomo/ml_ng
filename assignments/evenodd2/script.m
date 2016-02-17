7bacba99d046b501212746989221b4f371472736
Mario Rossi <veontomo@gmail.com>
2016-02-17 10:09:27 +0100

    feat: calculate method's accurancy, precision and recall

--- assignments/evenodd2/script.m
+++ assignments/evenodd2/script.m
@@ -32,13 +32,41 @@ theta_init = unifrnd(-2, 2, 1, size(X, 2));
 % Set Options
 options = optimset('GradObj', 'on', 'MaxIter', 100);
 
-cutoff = 20; % the number of examples to track
-Jpool = zeros(1, cutoff);
-for i = 1:cutoff
+trainingSize = 20; % the number of examples to train on
+Jpool = zeros(1, trainingSize);
+for i = 1:trainingSize
   [theta, J, exit_flag] = fminunc(@(Theta)(cost(X(1:i, :), Y(1:i, :), Theta)), theta_init, options);
   Jpool(i) = J;
 endfor
 
-plot(1:cutoff, Jpool)
+plot(1:trainingSize, Jpool)
+
+%%% Method precision, recall and accuracy
+printf("theta = ");
+theta
+tp = tn = fp = fn = 0;
+for i = 1:trainingSize
+  yPredicted = X(i, :) * theta' > 0;
+  if yPredicted == Y(i) 
+    if yPredicted == 1 
+      tp = tp + 1;
+    else 
+      tn = tn + 1;
+    endif;
+  else
+    if yPredicted == 1 
+      fp = fp + 1;
+    else 
+      fn = fn + 1;
+    endif;
+  endif;
+endfor;
+
+Prec = tp/(tp + fp)
+Rec = tp/(tp + fn)
+Acc = (tp + tn)/(tp + tn + fn + fp)
+
+
+
 
 
