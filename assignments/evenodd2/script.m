491f82c1f1a75291e1fb3fdf42d83a34363ec2af
Mario Rossi <veontomo@gmail.com>
2016-02-20 20:17:58 +0100

    feat: create a function that plots the cost function

--- assignments/evenodd2/script.m
+++ assignments/evenodd2/script.m
@@ -34,6 +34,9 @@ testSize = 100; % the number of test examples
 X = [ones(A, 1), DataNorm];
 
 % Training the model
+
+displayFlow(X, Y, trainingSize, testSize, 0);
+
 options = optimset('GradObj', 'on', 'MaxIter', 400);
 theta_init = unifrnd(0, 1, 1, size(X, 2));
 
@@ -46,8 +49,10 @@ Ytest = Y((trainingSize+1):(trainingSize + testSize));
 Jtraining = zeros(1, trainingSize);
 Jtest = zeros(1, trainingSize);
 Fscore = zeros(1, trainingSize);
+
+
 for i = 1:trainingSize
-  [theta, J, exit_flag] = fminunc(@(Theta)(cost(Xtraining(1:i, :), Ytraining(1:i, :), Theta, 0)), theta_init, options);
+  [theta, J, exit_flag] = fminunc(@(Theta)(cost(Xtraining(1:i, :), Ytraining(1:i, :), Theta, 10)), theta_init, options);
   J2 = cost(Xtest, Ytest, theta, 0);
   Jtest(i) = J2;
   Jtraining(i) = J;
@@ -58,20 +63,19 @@ for i = 1:trainingSize
   Rec = tp/(tp + fn);
   Acc = (tp + tn)/(tp + tn + fn + fp);
   Fscore(i) = 2*Prec*Rec/(Prec + Rec);
-  
 endfor
-
-plot(1:trainingSize, Jtraining, 'color', 'r')
+subplot (2, 1, 1)
 hold on;
+plot(1:trainingSize, Jtraining, 'color', 'r')
 plot(1:trainingSize, Jtest, 'color', 'k')
 hold off;
-
+subplot (2, 1, 2)
 plot(1:trainingSize, Fscore, 'color', 'b')
 
 
 
 %%%%%%%%%%%%% another model
-X = [ones(A, 1), DataNorm, mod(Data, 4)];
+X = [ones(A, 1), DataNorm, mod(Data, 2)];
 theta_init = unifrnd(-2, 2, 1, size(X, 2));
 
 
@@ -85,7 +89,7 @@ Jtraining = zeros(1, trainingSize);
 Jtest = zeros(1, trainingSize);
 Fscore = zeros(1, trainingSize);
 for i = 1:trainingSize
-  [theta, J, exit_flag] = fminunc(@(Theta)(cost(Xtraining(1:i, :), Ytraining(1:i, :), Theta, 0)), theta_init, options);
+  [theta, J, exit_flag] = fminunc(@(Theta)(cost(Xtraining(1:i, :), Ytraining(1:i, :), Theta, 3)), theta_init, options);
   J2 = cost(Xtest, Ytest, theta, 0);
   Jtest(i) = J2;
   Jtraining(i) = J;
@@ -99,12 +103,12 @@ for i = 1:trainingSize
 
 endfor
 
-plot(1:trainingSize, Jtraining, 'color', 'r')
+subplot (2, 1, 1)
 hold on;
+plot(1:trainingSize, Jtraining, 'color', 'r')
 plot(1:trainingSize, Jtest, 'color', 'k')
 hold off;
-
+subplot (2, 1, 2)
 plot(1:trainingSize, Fscore, 'color', 'b')
-
 
 
