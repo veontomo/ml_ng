04f2e03a8d1c43f56c612cec3866c60514d610ca
Mario Rossi <veontomo@gmail.com>
2016-03-01 11:19:00 +0100

    refactor: give more reasonable names to some variables

--- assignments/evenodd2/neuralCost.m
+++ assignments/evenodd2/neuralCost.m
@@ -2,11 +2,21 @@
 %% architecture
 %% X - training set (each row corresponds to a separate training set).
 %%     It is a matrix A x M, A - # training examples, M - # features
-%% Y - output. It is a vector A x 1.
-%% weights - list of weights. It is an unfolded version of all weights.
+%% Y - output matrix A x L, where A is the number of rows of X,
+%%     while L is a value of the last element of the row vector defining the 
+%%     network architecture.
+%% weights - row vector of weights. It is an unfolded version of all weights.
 %%         It includes weights for the bias units as well.
-%% layers - list of units in each layer.
+%% layers - row vector that defines the network architecture. Its elements are 
+%%         the number of units in corresponding layers.
 function [J grad] = neuralCost(X, Y, weights, layers)
+  %% calculates the number of all weights that a neural network with a given
+  %% architecture should have
+  requiredWeightNum = layers(2:end) * (1 + layers(1:end-1))';
+  weightsSize = size(weights, 2);
+  if (requiredWeightNum != weightsSize) 
+    printf("Warning: the network should have %u weights, while %u are given.\n", requiredWeightNum, weightsSize);
+  endif;
   J = [];
   %% for-loop iterates over the columns, while training examples 
   %% are arranged in rows. This is the reason for the transposition operation.
