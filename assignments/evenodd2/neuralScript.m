bdfe256648110d1bc5a16290543cd2066634157f
Mario Rossi <veontomo@gmail.com>
2016-03-07 15:30:31 +0100

    fix: transpose the gradient matrix before unfolding it (in Octave)

--- assignments/evenodd2/neuralScript.m
+++ assignments/evenodd2/neuralScript.m
@@ -1,7 +1,8 @@
-[J grad] = neuralCost([1 2; 3 4], [1; 0], -1:0.02:1, [2 3 1])
-
-
-[J grad] = neuralCost([6 7; 3 4], [1; 0], -1:0.02:1, [2 3 2 1])
+X = [3 4 1; 1 3 0; -2 5 3];
+Y = [1; 0; 1];
+params = -1:0.02:1;
+archit = [3 2 1];
+[J grad] = neuralCost(X, Y, params, archit)
 
 weightTotal = 20;
 archit = [2 3 2 1];
